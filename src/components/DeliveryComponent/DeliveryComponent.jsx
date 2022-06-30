2920316106caf71719828da37625b912f9d1b0c0
arinzehills <arinzehill@gmail.com>
2022-06-30 17:57:18 +0100

    updates

--- src/components/DeliveryComponent/DeliveryComponent.jsx
+++ src/components/DeliveryComponent/DeliveryComponent.jsx
@@ -1,10 +1,12 @@
+import { Icon } from "@iconify/react";
 import React from "react";
 import { useLocation } from "react-router-dom";
 import NavComponent from "../../pages/Dashboard/NavComponent/NavComponent";
-
+import NoDataFound from "../../pages/Dashboard/Request/NoDataFound";
+import TextArea from "../Inputfield/TextArea";
+import "./DeliveryComponent.css";
 const DeliveryComponent = () => {
   const location = useLocation();
-  console.log(location.state.item);
   return (
     <>
       <NavComponent
@@ -12,7 +14,18 @@ const DeliveryComponent = () => {
         // handleClick={handleClick}
         // setHandleNotData={setHandleNotData}
       />
-      <div>dadasdsada</div>
+      <div className="delivery_body">
+        <NoDataFound message={"No delivery yet"} />
+      </div>
+      <div className="delivery_upload_area">
+        <div className=" gradient send_delivery">
+          <Icon icon="ant-design:file-add-outlined" />
+        </div>
+        <TextArea label={"Write a comment..."} width={"60%"} />
+        <div className="gradient send_delivery">
+          <Icon icon="bi:send" />
+        </div>
+      </div>
     </>
   );
 };
