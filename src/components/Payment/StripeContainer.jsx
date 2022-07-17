6447b923a09d6f1045be5e6b4e7b1f364f2e43c2
arinzehills <arinzehill@gmail.com>
2022-07-17 23:26:51 +0100

    fixes

--- src/components/Payment/StripeContainer.jsx
+++ src/components/Payment/StripeContainer.jsx
@@ -5,7 +5,9 @@ import Payment from "./Payment";
 
 // const stripePromise = loadStripe(process.env.APP_STRIPE_PUBLIC_KEY);
 const stripePromise = loadStripe(
-  "pk_test_51KwinKEL7CF34u9QloS58YLEeNaPEUjdFdsIlqbcO5OPTyV2CGSgMmn5LZZGOACMRm64km6pKv6WTvgFgcv0ZXMh00EvCklSaW"
+  // "pk_test_51KwinKEL7CF34u9QloS58YLEeNaPEUjdFdsIlqbcO5OPTyV2CGSgMmn5LZZGOACMRm64km6pKv6WTvgFgcv0ZXMh00EvCklSaW"
+  // "pk_test_rYUYfsFV87GaHinUX9z4MQRT00OAmS5zla"
+  "pk_live_EiJ16xi6C0BvyvAq8HAVEEfz00hL15PmTI"
 );
 const StripeContainer = ({ next, back, planData }) => {
   return (
