730cf25fc387012c0a6e33c78a2eb76fa67e69ab
arinzehills <arinzehill@gmail.com>
2022-07-20 01:57:42 +0100

    chris

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Helmet } from "react-helmet";
 import { useNavigate } from "react-router-dom";
 import FormHero from "../../components/FormHero/FormHero";
 
